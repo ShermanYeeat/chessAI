9e38509aa48506ead188ad0e07cb61f1ce53acdb
Sherman <syee15467@gmail.com>
2020-09-01 19:52:49 -0700

    comments, cleaning up

--- ChessBoard.py
+++ ChessBoard.py
@@ -1,12 +1,10 @@
-class ChessBoard():
+class Board():
 
-    def __init__(self):
-        """
-        Chess Board representation as a 2D list
-            First character in the string represents the color (b for Black, w for White)
-            Second character is the piece (R for Rook, N for Knight)
-            -- represents no piece
-        """
+    def __init__(self):  
+        # Chess Board representation as a 2D list
+        #    First character in the string represents the color (b for Black, w for White)
+        #    Second character is the piece (R for Rook, N for Knight)
+        #    -- represents no piece
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
@@ -29,8 +27,8 @@ class ChessBoard():
 class Move():
 
     rankToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
-    rowsToRanks = {v: k for k, v in rankToRows.items()}
     filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    rowsToRanks = {v: k for k, v in rankToRows.items()}
     colsToFiles = {v: k for k, v in filesToCols.items()}
 
     def __init__(self, startSquare, endSquare, board):
@@ -42,7 +40,7 @@ class Move():
         self.pieceCaptured = board[self.endRow][self.endCol]
 
     def getChessNotation(self):
-        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+        return self.getRankAndFile(self.startRow, self.startCol) + self.getRankAndFile(self.endRow, self.endCol)
 
-    def getRankFile(self, row, col):
+    def getRankAndFile(self, row, col):
         return self.colsToFiles[col] + self.rowsToRanks[row]
--- main.py
+++ main.py
@@ -8,22 +8,16 @@ MAX_FPS = 30
 SQUARE_SIZE = 75
 IMAGES = {}
 
-def load_chess_pieces():
-    pieces = ["wR", "wN", "wB", "wQ", "wK", "wP", "bR", "bN", "bB", "bQ", "bK", "bP"]
-    for piece in pieces:
-        IMAGES[piece] = pyg.transform.scale(pyg.image.load("images/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
-
-
+# Main driver of the chess engine
 def main():
     pyg.init()
     window = pyg.display.set_mode((WIDTH, HEIGHT))
-    board = pyg.transform.scale(pyg.image.load("images/board.png"), (WIDTH, HEIGHT))
     clock = pyg.time.Clock()
-    game = ChessBoard.ChessBoard()
-    load_chess_pieces()
+    game = ChessBoard.Board() # creating the 2D list representation of the chess board
+    create_chess_pieces() 
     running = True
-    squareSelected = () # initially, no square is selected, but will keep track of the last click of the user
-    playerClicks = [] # keep track of player clicks i.e. [(1, 2), (1, 4)]
+    squareSelected = () # Will keep track of the last click of the user
+    playerClicks = [] # Keep track of player clicks in (row, col) format i.e. [(1, 2), (1, 4)]
     while running:
         for event in pyg.event.get():
             if event.type == pyg.QUIT:
@@ -32,38 +26,40 @@ def main():
                 location = pyg.mouse.get_pos()
                 col = location[0] // SQUARE_SIZE
                 row = location[1] // SQUARE_SIZE
-                if squareSelected == (row, col): # user clicked same square twice, do nothing
+                if squareSelected == (row, col): # User clicked same square twice, do nothing
                     squareSelected = () 
                     playerClicks = [] 
-                else:
+                else: # Add it to player clicks if a different square was clicked
                     squareSelected = (row, col)
                     playerClicks.append(squareSelected)
-                if len(playerClicks) == 2: # if player clicked twice
+                if len(playerClicks) == 2: # Player clicked two different squares
                     move = ChessBoard.Move(playerClicks[0], playerClicks[1], game.board)
                     print(move.getChessNotation())
                     game.makeMove(move)
                     squareSelected = ()
                     playerClicks = []
-
-        draw_game(window, game, board)
+        draw_game(window, game)
         clock.tick(MAX_FPS)
         pyg.display.flip()
 
+# Fitting chess piece images to the correct size relative to the chess board
+def create_chess_pieces():
+    pieces = ["wR", "wN", "wB", "wQ", "wK", "wP", "bR", "bN", "bB", "bQ", "bK", "bP"]
+    for piece in pieces:
+        IMAGES[piece] = pyg.transform.scale(pyg.image.load("images/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
 
-def draw_game(window, game, board):
-    window.blit(board, (0, 0))
+def draw_game(window, game):
+    chess_board = pyg.transform.scale(pyg.image.load("images/board.png"), (WIDTH, HEIGHT))
+    window.blit(chess_board, (0, 0))
     draw_pieces(window, game)
     pyg.display.update()
     
-    
-
 def draw_pieces(window, game):
     for row in range(DIMENSION):
         for col in range(DIMENSION):
             piece = game.board[row][col]
             if piece != "--":
                 window.blit(IMAGES[piece], (col * SQUARE_SIZE, row * SQUARE_SIZE))
-
 
 if __name__ == "__main__":
     main()
